d7ed538028728ba6ed3f4fb7921af2b79d325d87
Jastebsz <jastebsz@gmail.com>
2023-11-07 18:16:53 +0300

    123

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,8 +1,4 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
@@ -11,7 +7,34 @@ from flask import session,Blueprint
 #from apps.authentication.models import Users
 from geopy.geocoders import Nominatim
 from apps import db
-from apps.home.models import Worker
+from apps.home.models import Worker,Users,Full_tasks,Schedule,Scheduler
+
+from flask import request, jsonify
+
+
+
+@blueprint.route('/update_users', methods=['POST'])
+def update_users():
+    try:
+        data = request.json  # Получите данные, отправленные с фронтенда
+
+        # Пройдитесь по данным и обновите каждого пользователя в базе данных
+        for item in data:
+            user_id = item['id']
+            username = item['username']
+            role = item['role']
+
+            user = Users.query.get(user_id)
+            if user:
+                user.username = username
+                user.role = role
+            print('Hello')
+        db.session.commit()  # Сохраните изменения в базе данных
+
+        return jsonify({'message': 'Данные пользователей успешно обновлены'})
+    except Exception as e:
+            return jsonify({'error': 'Произошла ошибка при обновлении данных пользователей'}), 500
+
 def role():
     username = session.get('username')
     role = session.get('role')
@@ -25,50 +48,53 @@ def index():
 
     return render_template('home/index.html', segment='index', username=username, role=user_role,workers=workers)
 
-# @blueprint.route('/temlpate/tables',methods=["POST", "GET"])
-# @login_required
-# def tables():
-#     username, user_role = role()
-#     segment = get_segment(request)
-#     workers = Worker.query.all()  # Запрос к таблице Worker
-#     return render_template("home/tables.html", segment=segment, username=username, role=user_role, workers=workers)
+def workers_func():
+    workers=Worker.query.all()
+    # for worker in workers:
+    #             # Разбиваем координаты на широту и долготу
+    #     latitude, longitude = map(float, worker.location.split(', '))
+                    
+    #                 # Выполняем геокодирование
+    #     location_data = geolocator.reverse(f"{latitude}, {longitude}", language='ru', exactly_one=True)
+                    
+    #                 # Получаем адрес и обновляем поле location
+    #     if location_data:
+    #         address_elements = location_data.address.split(', ')
+    #                     # Переупорядочиваем элементы адреса в нужной последовательности
+    #         address = f"{address_elements[3]}, {address_elements[1]}, {address_elements[0]}"
+    #                     # Обновим поле location
+    #         worker.location = address
+    return workers
 
-# @blueprint.route('/billin', methods=['GET', 'POST'])
-# @login_required
-# def show_billing():
-#     username, user_role = role()
-#     return render_template('home/billing.html', segment='billing', username=username, role=user_role)
 
-# @blueprint.route('/calendar', methods=['GET', 'POST'])
-# @login_required
-# def show_calendar():
-#     username, user_role = role()
-#     return render_template('home/calendar.html', segment='calendar', username=username, role=user_role)
+# Здесь определите другие модели данных, если необходимо
 
-# @blueprint.route('/notifications')
-# @login_required
-# def show_notification():
-#     username, user_role = role()
-#     return render_template('home/notifications.html', segment='notifications', username=username, role=user_role)
+def find_task_data_by_idt(idt):
+    task = Full_tasks.query.filter_by(idt=idt).first()
+    if task:
+        # Вернуть данные о задаче в виде словаря
+        return {
+            "idt": task.idt,
+            "task_title":task.task_title,
+            "task_priority":task.task_priority,
+            "task_level":task.task_level,
+            "address": task.point_address,
+            "date":task.date
+            # Добавьте другие поля, если они нужны
+        }
+    else:
+        # Если задача с указанным idt не найдена, вернуть None или другое значение по умолчанию
+        return None
 
-# @blueprint.route('/profile')
-# @login_required
-# def show_profile():
-#     username, user_role = role()
-#     return render_template('home/profile.html', segment='profile', username=username, role=user_role)
 
-# @blueprint.route('/tables')  # Обновленный маршрут для страницы с таблицей
-# @login_required
-# def show_users():
-#     username, user_role = role()
-#     users = Users.query.all()
-#     # Получаем всех пользователей из базы данных
-#     return render_template('home/tables.html', segment='tables', username=username, role=user_role, users=users)  # Передаем список пользователей в шаблон
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="myGeocoder")
 @blueprint.route('/<template>', methods=['GET', 'POST'])
 @login_required
 def route_template(template):
+    workers=Worker.query.all()
+    users=Users.query.all()
+    full_tasks=Full_tasks.query.all()
     username,user_role=role() # Получите имя пользователя из сессии
     try:
         
@@ -78,44 +104,65 @@ def route_template(template):
         # Detect the current page
         segment = get_segment(request)
         if template == 'tables.html':
-            workers = Worker.query.all()[:3]
-            for worker in workers:
-                # Разбиваем координаты на широту и долготу
-                latitude, longitude = map(float, worker.location.split(', '))
-                
-                # Выполняем геокодирование
-                location_data = geolocator.reverse(f"{latitude}, {longitude}", language='ru', exactly_one=True)
-                
-                # Получаем адрес и обновляем поле location
-                if location_data:
-                    address_elements = location_data.address.split(', ')
-                    # Переупорядочиваем элементы адреса в нужной последовательности
-                    address = f"{address_elements[3]}, {address_elements[1]}, {address_elements[0]}"
-                    # Обновим поле location
-                    worker.location = address
+            workers=Worker.query.all()[:3]
+            users=Users.query.all()[:3]
+            full_tasks=Full_tasks.query.all()
+            schedule=Schedule.query.all()
+            
 
-            # Теперь у вас есть обновленные записи работников
-            return render_template("home/" + template, segment=segment, username=username, role=user_role, workers=workers)
+            # result_full = []
+            # task_user_mapping = {}
+
+            # for user in users:
+            #     for task in user.tasks:
+            #         task_user_mapping[task.idt] = user
+
+            # for key, idt in result:
+            #     if idt in task_user_mapping:
+            #         user = task_user_mapping[idt]
+            #         # Получаем данные о задаче
+            #         task_data = find_task_data_by_idt(idt)
+            #         if task_data:
+            #             new = Scheduler(
+            #                 id=key,
+            #                 task_title=task_data.task_title,
+            #                 task_priority=task_data.task_priority,
+            #                 task_level=task_data.task_level,
+            #                 point_address=task_data.point_address,
+            #                 status=task_data.status,
+            #                 username=user.username  # Добавляем имя пользователя
+            #             )
+            #             result_full.append(new)
+                
+            
+            
+            # for worker in workers:
+            #     # Разбиваем координаты на широту и долготу
+            #     latitude, longitude = map(float, worker.location.split(', '))
+                
+            #     # Выполняем геокодирование
+            #     location_data = geolocator.reverse(f"{latitude}, {longitude}", language='ru', exactly_one=True)
+                
+            #     # Получаем адрес и обновляем поле location
+            #     if location_data:
+            #         address_elements = location_data.address.split(', ')
+            #         # Переупорядочиваем элементы адреса в нужной последовательности
+            #         address = f"{address_elements[3]}, {address_elements[1]}, {address_elements[0]}"
+            #         # Обновим поле location
+            #         worker.location = address
+
+            # # Теперь у вас есть обновленные записи работников
+            return render_template("home/" + template, segment=segment, username=username, role=user_role, workers=workers,users=users)
         if template == 'workers.html':
-            workers = Worker.query.all()
-            for worker in workers:
-                # Разбиваем координаты на широту и долготу
-                latitude, longitude = map(float, worker.location.split(', '))
-                
-                # Выполняем геокодирование
-                location_data = geolocator.reverse(f"{latitude}, {longitude}", language='ru', exactly_one=True)
-                
-                # Получаем адрес и обновляем поле location
-                if location_data:
-                    address_elements = location_data.address.split(', ')
-                    # Переупорядочиваем элементы адреса в нужной последовательности
-                    address = f"{address_elements[3]}, {address_elements[1]}, {address_elements[0]}"
-                    # Обновим поле location
-                    worker.location = address
-
+            workers=workers_func()
             # Теперь у вас есть обновленные записи работников
             return render_template("home/" + template, segment=segment, username=username, role=user_role, workers=workers)
-
+        if template == 'users.html':
+            users=Users.query.all()
+            for user in users:
+                user.password='***'
+            # Теперь у вас есть обновленные записи работников
+            return render_template("home/" + template, segment=segment, username=username, role=user_role, users=users)
 
         # Serve the file (if exists) from app/templates/home/FILE.html
         else:
@@ -142,24 +189,3 @@ def get_segment(request):
 
     except:
         return None
-from flask import request, jsonify
-
-@blueprint.route('/update_worker/<int:worker_id>', methods=['POST'])
-def update_worker(worker_id):
-    # Получите данные из запроса
-    data = request.json
-
-    # Найдите сотрудника по ID
-    worker = Worker.query.get(worker_id)
-
-    if worker:
-        # Обновите данные сотрудника
-        for field, value in data.items():
-            setattr(worker, field, value)
-
-        # Сохраните изменения в базе данных
-        db.session.commit()
-
-        return jsonify({'message': 'Данные успешно обновлены'})
-    else:
-        return jsonify({'error': 'Сотрудник не найден'}), 404
