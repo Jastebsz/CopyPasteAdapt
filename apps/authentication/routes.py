12548758ed04d658ab7941095c82ad7bf9171373
Jastebsz <jastebsz@gmail.com>
2023-11-08 20:04:36 +0300

    big_pull

---
+++ apps/authentication/routes.py
@@ -0,0 +1,149 @@
+# -*- encoding: utf-8 -*-
+from flask import render_template, redirect, request, url_for
+from flask_login import (
+    current_user,
+    login_user,
+    logout_user
+)
+
+from apps import db, login_manager
+from apps.authentication import blueprint
+from apps.authentication.forms import LoginForm, CreateAccountForm
+from apps.authentication.models import Users,Worker
+from flask import session
+from apps.authentication.util import verify_pass
+
+import re
+
+def role1():
+    username = session.get('username')
+    role = session.get('role')
+    return username, role
+
+
+
+@blueprint.route('/')
+def route_default():
+    return redirect(url_for('authentication_blueprint.login'))
+
+
+# Login & Registration
+
+@blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm(request.form)
+    if 'login' in request.form:
+        # Read form data
+        username = request.form['username']
+        password = request.form['password']
+
+        # Locate user
+        user = Users.query.filter_by(username=username).first()
+
+        # Check if the user exists in the database
+        if user:
+            # Check the password
+            if verify_pass(password, user.password):
+                session['username'] = user.username  # Save the username in the session
+                session['role'] = user.role
+                login_user(user)
+
+                # Check if the user is an admin
+                if user.role == 'admin' and user is not None:
+                    return redirect(url_for('authentication_blueprint.route_default'))
+                elif user.role != 'admin':
+                    # User is not an admin, show a message
+                    return render_template('accounts/login.html',
+                                           msg='У вас недостаточно прав для доступа',
+                                            form=login_form)
+                else:    
+                    # В случае, когда пользователь не существует
+                    redirect(url_for('home_blueprint.index'))
+            # Password is incorrect
+            return render_template('accounts/login.html',
+                                   msg='Неверное имя пользователя или пароль',
+                                   form=login_form)
+
+        # User doesn't exist in the database
+        return render_template('accounts/login.html',
+                               msg='Обратитесь к администратору для создания аккаунта',
+                               form=login_form)
+
+    if not current_user.is_authenticated:
+        return render_template('accounts/login.html',
+                               form=login_form)
+
+    return redirect(url_for('home_blueprint.index'))
+
+
+
+@blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    username = session.get('username')
+    user_role = request.form.get('role')
+    create_account_form = CreateAccountForm(request.form)
+    fio_column1 = Worker.query.with_entities(Worker.FIO).all()
+    fio_column=[]
+    for i in fio_column1:
+        fio_column.append(i[0])
+        print(fio_column)
+    if 'register' in request.form:
+        print(request.form)
+        new_username = request.form['username']
+        role = request.form['role']
+        new_worker_FIO=request.form['worker_FIO']
+        # Check if the new username exists
+        user = Users.query.filter_by(username=new_username).first()
+        if user:
+            return render_template('accounts/register.html',
+                                   msg='Аккаунт уже существует',
+                                   success=False,
+                                   form=create_account_form, username=username, role=user_role,fio_column=fio_column)
+        user = Users.query.filter_by(worker_FIO=new_worker_FIO).first()
+        if user:
+            return render_template('accounts/register.html',
+                                   msg='Аккаунт уже существует',
+                                   success=False,
+                                   form=create_account_form, username=username, role=user_role,fio_column=fio_column)
+
+        # Create the user with the new username
+        user = Users(**request.form)
+        db.session.add(user)
+        db.session.commit()
+
+        return render_template('accounts/register.html',
+                               msg='Аккаунт успешно создан',
+                               success=True,
+                               form=create_account_form, username=username, role=user_role)
+
+    else:
+        return render_template('accounts/register.html', form=create_account_form, username=username, role=user_role,fio_column=fio_column)
+
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('authentication_blueprint.login'))
+
+
+# Errors
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('home/page-403.html'), 403
+
+
+@blueprint.errorhandler(403)
+def access_forbidden(error):
+    return render_template('home/page-403.html'), 403
+
+
+@blueprint.errorhandler(404)
+def not_found_error(error):
+    return render_template('home/page-404.html'), 404
+
+
+@blueprint.errorhandler(500)
+def internal_error(error):
+    return render_template('home/page-500.html'), 500
---
+++ apps/home/models.py
@@ -0,0 +1,149 @@
+from apps import db # Import the SQLAlchemy database instance
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() 
+
+class Worker(db.Model):
+    __tablename__ = 'Worker'
+
+    id = db.Column(db.Integer, primary_key=True)
+    FIO = db.Column(db.String(64), unique=True)
+    location = db.Column(db.String(64))
+    grade = db.Column(db.String(128))  # Store the hashed password as a string
+
+    def __init__(self, FIO, location, grade):
+        self.FIO = FIO
+        self.location = location
+        self.grade = grade
+    def __repr__(self):
+        return '<Worker {}>'.format(self.body)  
+    
+class Tasks(db.Model):
+    __tablename__ = 'Tasks'
+    type = db.Column(db.Integer, primary_key=True)                      # тип
+    title = db.Column(db.String(100), nullable=False)                   # название
+    priority = db.Column(db.String(30), nullable=False)                 # приоритет
+    lead_time = db.Column(db.Float, nullable=False)                     # время
+    condition = db.Column(db.String(120), nullable=False)               # условие
+    level = db.Column(db.String(30), nullable=False) 
+
+    def __init__(self,type, title, priority, lead_time,condition,level):
+        self.type = type
+        self.title = title
+        self.priority = priority
+        self.lead_time=lead_time
+        self.condition=condition
+        self.level = level
+    def __repr__(self):
+        return '<Tasks {}>'.format(self.body) 
+class Points(db.Model):
+    __tablename__ = 'Points'
+    id = db.Column(db.Integer, primary_key=True)                        # номер точки
+    address = db.Column(db.String(100), nullable=False)                 # адрес точки
+    connected = db.Column(db.String(80), nullable=False)                # когда подключена точка
+    delivered = db.Column(db.Boolean, default=False)                    # карты и материалы доставлены
+    days_last_card = db.Column(db.Integer)                              # кол-во дней после выдачи последней карты
+    num_approved_app = db.Column(db.Integer)                            # кол-во одобренных заявок
+    num_card = db.Column(db.Integer)                                    # кол-во выданных карт
+    def __init__(self, id, address, connected,delivered,days_last_card,num_approved_app,num_card):
+        self.id = id
+        self.address = address
+        self.connected = connected
+        self.delivered = delivered
+        self.days_last_card = days_last_card
+        self.num_approved_app = num_approved_app
+        self.num_card=num_card
+    def __repr__(self):
+        return '<Points {}>'.format(self.body)     
+
+
+class Full_tasks(db.Model):
+    __tablename__ = 'Full_tasks'
+    idt = db.Column(db.String(100), primary_key=True)                      # тип
+    task_type = db.Column(db.Integer, primary_key=True)                   # название
+    task_title = db.Column(db.String(100), nullable=False)                 # приоритет
+    task_priority = db.Column(db.String(30), nullable=False)                     # время
+    task_lead_time = db.Column(db.Float, nullable=False)               # условие
+    task_level = db.Column(db.String(30), nullable=False) 
+    point_id=db.Column(db.Integer, primary_key=True)
+    point_address=db.Column(db.String(100), nullable=False)
+    date=db.Column(db.String(10), nullable=False)
+    status=db.Column(db.String(10), nullable=False)
+    
+    
+    def __init__(self,idt, task_type, task_title, task_priority,task_lead_time,task_level,point_id,point_address):
+        self.idt = idt                     
+        self.task_type =task_type         
+        self.task_title =task_title
+        self.task_priority =task_priority                  
+        self.task_lead_time =task_lead_time    
+        self.task_level =task_level 
+        self.point_id=point_id
+        self.point_address=point_address
+
+    def __repr__(self):
+        return '<Full_tasks {}>'.format(self.body) 
+
+    
+    
+
+class Users(db.Model):
+
+    __tablename__ = 'Users'
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    role = db.Column(db.String(64), unique=True)
+    worker_FIO = db.Column(db.Integer)
+    password = db.Column(db.LargeBinary)
+    
+
+    # def __init__(self, username, role, password):
+    #     self.username = username
+    #     self.role = role
+    #     self.password = password
+        
+    # def __repr__(self):
+    #     return '<Users {}>'.format(self.username) 
+
+
+
+
+
+import json
+from sqlalchemy.ext.mutable import Mutable
+
+class JSONEncodedDict(db.TypeDecorator):
+    impl = db.String(64)
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+class Schedule(db.Model):
+    __tablename__ = 'Schedule'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(64), unique=True)
+    schedule = db.Column(JSONEncodedDict)
+
+    def __init__(self, id,date,shedule):
+        self.id = id
+        self.address = date
+        self.data = shedule
+
+    def __repr__(self):
+        return '<Schedule {}>'.format(self.id)
+    
+    
+class Scheduler():
+    def __init__(self, id,task_title,task_priority,task_level,point_address,status):
+        self.id = id                             
+        self.task_title =task_title
+        self.task_priority =task_priority                   
+        self.task_level =task_level 
+        self.point_address=point_address
